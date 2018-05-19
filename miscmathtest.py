c7386a9cfce32b1cbdba10aab458fda02096f250
Armend <armend.veseli@yahoo.com>
2018-05-19 03:33:40 -0400

    Create miscmathtest.py
    Test miscmath functions

---
+++ miscmathtest.py
@@ -0,0 +1,11 @@
+import miscmath as mm
+
+print(mm.avg([1,2,3,4]))
+
+print(mm.std_dev([2,4,4,4,5,5,7,9], True))
+
+print(mm.std_dev([727.7,1086.5,1091.0,1361.3,1490.5,1956.1]))
+
+print(mm.std_norm_distrib(1))
+
+print(mm.general_norm_distrib(1,1,1))
