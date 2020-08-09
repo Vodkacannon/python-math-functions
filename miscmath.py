12d23428a5cfda5a3b3528e9e15948b2e3aec428
Armend <armend.veseli@yahoo.com>
2020-08-09 01:36:08 -0400

    Changed tabs to 4 spaces.

--- miscmath.py
+++ miscmath.py
@@ -4,27 +4,27 @@ from math import exp
 
 
 def avg(iterator):
-	return sum(iterator) / len(iterator)
+    return sum(iterator) / len(iterator)
 
 
 def std_dev(iterator, population = False):
-	_avg = avg(iterator)
-	std_dev = 0
+    _avg = avg(iterator)
+    std_dev = 0
 
-	for i in range(len(iterator)):
-		std_dev += (iterator[i] - _avg) * (iterator[i] - _avg)
+    for i in range(len(iterator)):
+        std_dev += (iterator[i] - _avg) * (iterator[i] - _avg)
 
 	if population:
-		return sqrt(std_dev / (len(iterator)))
+	    return sqrt(std_dev / (len(iterator)))
 
-	return sqrt(std_dev / (len(iterator)-1))
+    return sqrt(std_dev / (len(iterator)-1))
 
 
 def std_norm_distrib(x):
-	return 1 / sqrt(2 * pi) * exp(-.5 * x * x)
+    return 1 / sqrt(2 * pi) * exp(-.5 * x * x)
 
 
 def general_norm_distrib(x, micro, sigma):
-	sigma *= sigma
+    sigma *= sigma
 
-return 1 / sigma * std_norm_distrib((x - micro) / sigma)
+    return 1 / sigma * std_norm_distrib((x - micro) / sigma)
